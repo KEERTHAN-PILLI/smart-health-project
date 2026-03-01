a02ebf31067fb99a0a9b62a7be937191ff0756d0
PILLI KEERTHAN BABU <142890676+KEERTHAN-PILLI@users.noreply.github.com>
2026-03-01 23:09:31 +0530

    Add Google OAuth Provider to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./styles/auth.css";
 
+// Replace with your actual Google Client ID
+const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
